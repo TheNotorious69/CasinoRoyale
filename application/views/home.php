a2b7b78565849cb70e1270f1d90ce69d696d0a6b
MarcoCazares <marcocazaresmtz471@gmail.com>
2022-11-03 15:08:13 -0600

    Update home.php

--- application/views/home.php
+++ application/views/home.php
@@ -34,7 +34,7 @@
           <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="#">Action</a></li>
             <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">Holaaa</a></li>
           </ul>
         </li>
       </ul>
