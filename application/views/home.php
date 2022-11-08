9561537b28b2014ab9a94e01e07e43b951ba02a2
TheNotorious69 <019001329@upsrj.edu.mx>
2022-11-07 23:22:05 -0600

    Update home.php

--- application/views/home.php
+++ application/views/home.php
@@ -12,7 +12,7 @@
 <nav class="navbar navbar-expand-lg" style="background-color: #423C7A;">
   <div class="container-fluid">
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link" aria-current="page" href="#" style="color: #9B9B9B;">
             Juegos 
@@ -22,35 +22,38 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#" style="color: #9B9B9B;">Features</a>
+          <a class="nav-link" href="#" style="color: #9B9B9B;" >Juegos Gratis</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#" style="color: #9B9B9B;">Pricing</a>
+          <a class="nav-link" href="#" style="color: #9B9B9B;">Noticias</a>
         </li>
-        <li class="nav-item dropdown" >
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: #9B9B9B;">
-            Dropdown link
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+        <li class="nav-item">
+          <a class="nav-link" href="#" style="color: #9B9B9B;">Promociones</a>
         </li>
+      </ul>
+      <ul class="nav justify-content-end">
+         <li class="nav-item dropdown" >
+            <div class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: #9B9B9B;">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Bandera_de_Espa%C3%B1a.svg/1200px-Bandera_de_Espa%C3%B1a.svg.png" style="height: 25px">
+            </div>
+         </li>
       </ul>
     </div>
   </div>
 </nav>
 
 
-   
   
 
-
-
-
-
-
+<nav class="navbar navbar-expand-lg" style="background-color: #423C7A;" class="nav justify-content-center">
+   <ul class="nav justify-content-center" style="backgroud-color: #302A58;">
+      <li class="nav-item">
+         <div class="nav-item" href="#" role="button" aria-expanded="false" style="color: #DDBF4C;">
+            <img src="https://roobet.com/images/logo.svg" style="height: 100px">
+         </div>
+      </li>
+   </ul>
+</nav>
 
 
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
