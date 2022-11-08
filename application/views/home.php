f046c5b0c1cc3b3075e98ec3ade20f72f7bb13ab
MarcoCazares <marcocazaresmtz471@gmail.com>
2022-11-08 15:27:02 -0600

    Update home.php

--- application/views/home.php
+++ application/views/home.php
@@ -73,6 +73,7 @@
    </ul>
 </nav>
 
+<<<<<<< Updated upstream
 <!-- <div class="card" style="width: 18rem;" >
   
   <a href="<?=site_url('maquinitas')?>"><img src="https://megaricos.com/wp-content/uploads/2021/05/shutterstock_1231143874.jpg" class="card-img-top" style="width: 600px"></a>
@@ -83,6 +84,12 @@
       <img src="https://megaricos.com/wp-content/uploads/2021/05/shutterstock_1231143874.jpg" style="border-radius: 12px; height:150px; width: 550px" class="card-img-top" alt="...">
    </div>
   
+=======
+<div class="card" style="width: 18rem;" >
+  <img src="https://megaricos.com/wp-content/uploads/2021/05/shutterstock_1231143874.jpg" class="card-img-top" alt="...">
+  <a href="<?=site_url('maquinitas')?>">Maquinitas</a>
+</div>
+>>>>>>> Stashed changes
 
 
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
