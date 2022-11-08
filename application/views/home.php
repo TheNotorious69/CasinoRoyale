79f228c88dc0380a73279f64058933b484d10a8e
TheNotorious69 <019001329@upsrj.edu.mx>
2022-11-08 15:27:20 -0600

    gg

--- application/views/home.php
+++ application/views/home.php
@@ -5,6 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
+<<<<<<< Updated upstream
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body style="background-color: #322C5C">
@@ -41,6 +42,20 @@
     </div>
   </div>
 </nav>
+=======
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="home.css">
+</head>
+<body class="home">
+
+<nav class="navbar" style="background-color: #423C7A;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Juegos</a>
+  </div>
+</nav>
+  
+   
+>>>>>>> Stashed changes
 
 
   
