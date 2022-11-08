998d883b2f43d1e81809934e429507a0ad8af1fc
MarcoCazares <marcocazaresmtz471@gmail.com>
2022-11-08 01:08:28 -0600

    Update maquinitas.php

--- application/views/maquinitas.php
+++ application/views/maquinitas.php
@@ -1,11 +1,67 @@
-<!DOCTYPE html>
-<html>
+<!DOCTYPE html class="home">
+<html lang="en">
 <head>
-	<title>403 Forbidden</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Maquinitas</title>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
+
 <body>
 
-<p>Maquinitas</p>
+
+<nav class="navbar navbar-expand-lg" style="background-color: #423C7A;">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#" style="color: #9B9B9B;">
+            Juegos 
+            <span class="badge text-bg-danger" style="background-color: #F45555;">
+               En Vivo
+            </span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" style="color: #9B9B9B;" >Juegos Gratis</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="maquinitas" style="color: #9B9B9B;">Noticias</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" style="color: #9B9B9B;">Promociones</a>
+        </li>
+      </ul>
+      <ul class="nav justify-content-end">
+         <li class="nav-item dropdown" >
+            <div class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: #9B9B9B;">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Bandera_de_Espa%C3%B1a.svg/1200px-Bandera_de_Espa%C3%B1a.svg.png" style="height: 25px">
+            </div>
+         </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+  
+
+<nav class="navbar navbar-expand-lg" style="background-color: #423C7A;" class="nav justify-content-center">
+   <ul class="nav justify-content-center" style="backgroud-color: #302A58;">
+      <li class="nav-item">
+         <div class="nav-item" href="<?=site_url('maquinitas')?>" role="button" aria-expanded="false" style="color: #DDBF4C;">
+            <img src="https://roobet.com/images/logo.svg" style="height: 100px">
+         </div>
+      </li>
+   </ul>
+</nav>
+
+
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+
 
 </body>
 </html>
