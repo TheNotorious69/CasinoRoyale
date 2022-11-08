5e85b9206efe06762ab854ac8833b08223d61593
MarcoCazares <marcocazaresmtz471@gmail.com>
2022-11-08 13:41:14 -0600

    Update maquinitas.php

--- application/views/maquinitas.php
+++ application/views/maquinitas.php
@@ -44,9 +44,6 @@
   </div>
 </nav>
 
-
-  
-
 <nav class="navbar navbar-expand-lg" style="background-color: #302A58;" class="nav justify-content-center">
    <ul class="nav justify-content-center" style="backgroud-color: #302A58;">
       <li class="nav-item">
@@ -76,10 +73,17 @@
 </nav>
 
 
-   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+
+
+
+  
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
 
-
 </body>
+
+
 </html>
