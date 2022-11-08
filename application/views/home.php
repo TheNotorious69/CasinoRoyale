bf9c9bbc0eb03b9f299cb0bc7e8ed624a65f7fcb
TheNotorious69 <019001329@upsrj.edu.mx>
2022-11-07 22:22:24 -0600

    Update home.php

--- application/views/home.php
+++ application/views/home.php
@@ -6,13 +6,43 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-   <link href="D:\xampp\htdocs\CasinoRoyale\application\views\home.css"
-      rel="stylesheet" type="text/css">
 </head>
 <body>
-<nav  class="nav">
-  
-  </nav>
+
+<nav class="navbar navbar-expand-lg" style="background-color: #423C7A;">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#" style="color: #9B9B9B;">
+            Juegos 
+            <span class="badge text-bg-danger" style="background-color: #F45555;">
+               En Vivo
+            </span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" style="color: #9B9B9B;">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" style="color: #9B9B9B;">Pricing</a>
+        </li>
+        <li class="nav-item dropdown" >
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: #9B9B9B;">
+            Dropdown link
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
    
   
 
