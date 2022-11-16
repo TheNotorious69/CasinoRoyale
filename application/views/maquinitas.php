07a349fbcee549d46d36df09856b93e440216665
MarcoCazares <marcocazaresmtz471@gmail.com>
2022-11-15 23:26:04 -0600

    Update maquinitas.php

--- application/views/maquinitas.php
+++ application/views/maquinitas.php
@@ -8,7 +8,7 @@
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
-<body>
+<body style="background-color: #322C5C">
 
 
 <nav class="navbar navbar-expand-lg" style="background-color: #423C7A;">
@@ -64,8 +64,8 @@
       <div style="color: #9B9B9B; margin-top: 30px">
          <img src="https://icones.pro/wp-content/uploads/2021/06/icone-loupe-gris.png" style="height: 40px;">
              Buscar Juegos
-         <img src="https://cdn-icons-png.flaticon.com/512/565/565422.png" style="height: 40px; background-color: #DDBF4C; margin-left: 20px; border-color: rgb(0, 0, 0); border-radius: 20px" class="img-thumbnail">
-         <img src="https://icons.veryicon.com/png/o/internet--web/55-common-web-icons/person-4.png" style="height: 40px; background-color: #DDBF4C; margin-left: 20px; border-color: rgb(0, 0, 0); border-radius: 20px" class="img-thumbnail">
+         <img src="https://cdn-icons-png.flaticon.com/512/565/565422.png" style="height: 40px; background-color: #DDBF4C; margin-left: 20px; border-color: rgb(0, 0, 0); border-radius: 20px">
+         <img src="https://icons.veryicon.com/png/o/internet--web/55-common-web-icons/person-4.png" style="height: 40px; background-color: #DDBF4C; margin-left: 20px; border-color: rgb(0, 0, 0); border-radius: 20px">
       </div>
          
       </li>
@@ -73,77 +73,72 @@
 </nav>
 <div  style="display: flex; ">
    <div style="padding: 20px; margin: auto">
-   <img src="https://roobet.com/cdn-cgi/image/dpr=1.25,width=200,height=auto,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/pragmatic:vs20fruitsw-kugK-iJVB.jpeg" class="img-thumbnail" style="height: 200px; weigth:200px; border-radius: 20px;">
-   <h5 style="text-align: center;">Juicy Fruits</h5>
+   <img src="https://roobet.com/cdn-cgi/image/dpr=1,width=200,height=auto,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/pragmatic:vs50juicyfr-y819M1NX9.jpeg" style="height: 200px; weigth:200px; border-radius: 20px;">
+   <h5 style="text-align: center; color: #FFFFFF;">Juicy Fruits</h5>
    </div>
    <div style="padding: 20px; margin: auto">
-   <img src="https://roobet.com/cdn-cgi/image/dpr=1.25,width=200,height=auto,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/pragmatic:vs20fruitsw-kugK-iJVB.jpeg" class="img-thumbnail" style="height: 200px; weigth:200px; border-radius: 20px; ">
-      <h5 style="text-align: center;"></h5>
+   <img src="https://roobet.com/cdn-cgi/image/dpr=1,width=200,height=auto,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/relax:MoneyTrain-mEetB84br.jpeg" style="height: 200px; weigth:200px; border-radius: 20px; ">
+      <h5 style="text-align: center; color: #FFFFFF;">Mony Train</h5>
    </div>
    <div style="padding: 20px; margin: auto">
-   <img src="https://roobet.com/cdn-cgi/image/dpr=1.25,width=200,height=auto,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/pragmatic:vs20fruitsw-kugK-iJVB.jpeg" class="img-thumbnail" style="height: 200px; weigth:200px; border-radius: 20px;">
-      <h5 style="text-align: center;">Juego1</h5>
+   <img src="https://roobet.com/cdn-cgi/image/dpr=1,width=200,height=auto,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/pushgaming:BigBamboo-yqe4envbO.jpeg" style="height: 200px; weigth:200px; border-radius: 20px;">
+      <h5 style="text-align: center; color: #FFFFFF;">Big Bamboo</h5>
    </div>
    <div style="padding: 20px; margin: auto">
-   <img src="https://roobet.com/cdn-cgi/image/dpr=1.25,width=200,height=auto,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/pragmatic:vs20fruitsw-kugK-iJVB.jpeg" class="img-thumbnail" style="height: 200px; weigth:200px; border-radius: 20px;">
-      <h5 style="text-align: center;">Juego1</h5>
+   <img src="https://roobet.com/cdn-cgi/image/dpr=1.25,width=200,height=auto,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/pragmatic:vs20fruitsw-kugK-iJVB.jpeg" style="height: 200px; weigth:200px; border-radius: 20px;">
+      <h5 style="text-align: center; color: #FFFFFF;">Gates Of Olympus</h5>
    </div>
    <div style="padding: 20px; margin: auto">
-   <img src="https://roobet.com/cdn-cgi/image/dpr=1.25,width=200,height=auto,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/pragmatic:vs20fruitsw-kugK-iJVB.jpeg" class="img-thumbnail" style="height: 200px; weigth:200px; border-radius: 20px;">
-      <h5 style="text-align: center;">Juego1</h5>
+   <img src="https://roobet.com/cdn-cgi/image/dpr=1,width=200,height=auto,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/pragmatic:vswaysrhino-lJzD6DQnV.jpeg" style="height: 200px; weigth:200px; border-radius: 20px;">
+      <h5 style="text-align: center; color: #FFFFFF;">Great Rhino Megaways</h5>
    </div>
 </div>
 
 <div  style="display: flex; ">
    <div style="padding: 20px; margin: auto">
-   <img src="https://roobet.com/cdn-cgi/image/dpr=1.25,width=200,height=auto,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/pragmatic:vs20fruitsw-kugK-iJVB.jpeg" class="img-thumbnail" style="height: 200px; weigth:200px; border-radius: 20px;">
-      <h5 style="text-align: center;">Juego1</h5>
+   <img src="https://roobet.com/cdn-cgi/image/dpr=1,width=200,height=auto,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/pragmatic:vs10bbbonanza-LV-fUyHL7.jpeg" style="height: 200px; weigth:200px; border-radius: 20px;">
+      <h5 style="text-align: center; color: #FFFFFF; ">Big Bass Bonoza </h5>
    </div>
    <div style="padding: 20px; margin: auto">
-   <img src="https://roobet.com/cdn-cgi/image/dpr=1.25,width=200,height=auto,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/pragmatic:vs20fruitsw-kugK-iJVB.jpeg" class="img-thumbnail" style="height: 200px; weigth:200px; border-radius: 20px; ">
-      <h5 style="text-align: center;">Juego1</h5>
+   <img src="https://roobet.com/cdn-cgi/image/dpr=1,width=200,height=auto,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/pragmatic:vs25mustang-N3-vD820V.jpeg" style="height: 200px; weigth:200px; border-radius: 20px; ">
+      <h5 style="text-align: center; color: #FFFFFF; ">Mustang Gold</h5>
    </div>
    <div style="padding: 20px; margin: auto">
-   <img src="https://roobet.com/cdn-cgi/image/dpr=1.25,width=200,height=auto,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/pragmatic:vs20fruitsw-kugK-iJVB.jpeg" class="img-thumbnail" style="height: 200px; weigth:200px; border-radius: 20px;">
-      <h5 style="text-align: center;">Juego1</h5>
+   <img src="https://roobet.com/cdn-cgi/image/dpr=1,width=200,height=auto,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/pushgaming:mysterymuseum-jpLX4O8FU.jpeg" style="height: 200px; weigth:200px; border-radius: 20px;">
+      <h5 style="text-align: center; color: #FFFFFF; ">Mystery Musseum</h5>
    </div>
    <div style="padding: 20px; margin: auto">
-   <img src="https://roobet.com/cdn-cgi/image/dpr=1.25,width=200,height=auto,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/pragmatic:vs20fruitsw-kugK-iJVB.jpeg" class="img-thumbnail" style="height: 200px; weigth:200px; border-radius: 20px;">
-      <h5 style="text-align: center;">Juego1</h5>
+   <img src="https://roobet.com/cdn-cgi/image/dpr=1,width=200,height=auto,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/playngo:riseofmerlin-aRs-46wuC.jpeg" style="height: 200px; weigth:200px; border-radius: 20px;">
+      <h5 style="text-align: center; color: #FFFFFF; ">Rise of Merlin</h5>
    </div>
    <div style="padding: 20px; margin: auto">
-   <img src="https://roobet.com/cdn-cgi/image/dpr=1.25,width=200,height=auto,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/pragmatic:vs20fruitsw-kugK-iJVB.jpeg" class="img-thumbnail" style="height: 200px; weigth:200px; border-radius: 20px;">
-      <h5 style="text-align: center;">Juego1</h5>
+   <img src="https://roobet.com/cdn-cgi/image/dpr=1,width=200,height=auto,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/relax:SnakeArena-K7x_sq7Z_.jpeg" style="height: 200px; weigth:200px; border-radius: 20px;">
+      <h5 style="text-align: center; color: #FFFFFF; ">Snake Arena</h5>
    </div>
 </div>
 
 <div  style="display: flex; ">
    <div style="padding: 20px; margin: auto">
-   <img src="https://roobet.com/cdn-cgi/image/dpr=1.25,width=200,height=auto,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/pragmatic:vs20fruitsw-kugK-iJVB.jpeg" class="img-thumbnail" style="height: 200px; weigth:200px; border-radius: 20px;">
-      <h5 style="text-align: center;">Juego1</h5>
+   <img src="https://roobet.com/cdn-cgi/image/dpr=1,width=200,height=auto,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/nolimit:MentalDX1-wUEByc7aC.jpeg" style="height: 200px; weigth:200px; border-radius: 20px;">
+      <h5 style="text-align: center; color: #FFFFFF; ">Mental</h5>
    </div>
    <div style="padding: 20px; margin: auto">
-   <img src="https://roobet.com/cdn-cgi/image/dpr=1.25,width=200,height=auto,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/pragmatic:vs20fruitsw-kugK-iJVB.jpeg" class="img-thumbnail" style="height: 200px; weigth:200px; border-radius: 20px; ">
-      <h5 style="text-align: center;">Juego1</h5>
+   <img src="https://roobet.com/cdn-cgi/image/dpr=1,width=200,height=auto,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/softswiss:AztecMagicDeluxe-VNpu0Ww_cE.jpg" style="height: 200px; weigth:200px; border-radius: 20px; ">
+      <h5 style="text-align: center; color: #FFFFFF;">Aztec Magic Deluxe </h5>
    </div>
    <div style="padding: 20px; margin: auto">
-   <img src="https://roobet.com/cdn-cgi/image/dpr=1.25,width=200,height=auto,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/pragmatic:vs20fruitsw-kugK-iJVB.jpeg" class="img-thumbnail" style="height: 200px; weigth:200px; border-radius: 20px;">
-      <h5 style="text-align: center;">Juego1</h5>
+   <img src="https://roobet.com/cdn-cgi/image/dpr=1,width=200,height=auto,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/relax:ExtraChilli-FlFc1liL-.jpeg" style="height: 200px; weigth:200px; border-radius: 20px;">
+      <h5 style="text-align: center; color: #FFFFFF; ">Extra Chilli</h5>
    </div>
    <div style="padding: 20px; margin: auto">
-   <img src="https://roobet.com/cdn-cgi/image/dpr=1.25,width=200,height=auto,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/pragmatic:vs20fruitsw-kugK-iJVB.jpeg" class="img-thumbnail" style="height: 200px; weigth:200px; border-radius: 20px;">
-      <h5 style="text-align: center;">Juego1</h5>
+   <img src="https://roobet.com/cdn-cgi/image/dpr=1,width=81,height=81,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/softswiss:BeerBonanza-cENqLhir8i.png" style="height: 200px; weigth:200px; border-radius: 20px;">
+      <h5 style="text-align: center; color: #FFFFFF;">Beer Bonaza</h5>
    </div>
    <div style="padding: 20px; margin: auto">
-   <img src="https://roobet.com/cdn-cgi/image/dpr=1.25,width=200,height=auto,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/pragmatic:vs20fruitsw-kugK-iJVB.jpeg" class="img-thumbnail" style="height: 200px; weigth:200px; border-radius: 20px;">
-      <h5 style="text-align: center;">Juego1</h5>
+   <img src="https://roobet.com/cdn-cgi/image/dpr=1,width=81,height=81,quality=50,metadata=none,format=auto/https://roobet-public.s3.amazonaws.com/pragmatic:vs25wolfjpt-uRP348VI1.jpeg" style="height: 200px; weigth:200px; border-radius: 20px;">
+      <h5 style="text-align: center; color: #FFFFFF;">Wolf Gold Pawer Jackpot</h5>
    </div>
 </div>
-
-
-
-
-
 
 
 
